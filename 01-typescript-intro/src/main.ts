48379149ae88ba2e90c077d6815bc42d43380562
Christian <christiancatamuscay@r-fast.com>
2023-07-11 15:50:26 -0500

    Sección 3: Bases de TypeScript - Recomendado - 26. Genéricos

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 // import './topics/01-basic-types';
 // import './topics/02-object-interfaces';
 //import './topics/07-import-export'
-import './topics/08-classes';
+import './topics/09-generics';
 
 const app = document.querySelector<HTMLDivElement>("#app");
 

fb9925c86e03ddb3e2406e99f57a46e672235f14
Christian <christiancatamuscay@r-fast.com>
2023-07-11 11:48:53 -0500

    Sección 3: Bases de TypeScript - Recomendado - 12. Tipos básicos y conceptos generales

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+
+
+const name:string = 'Christian';
+let hpPoint:number|'FULL' = 95;
+const isAlive:boolean = true;
+
+hpPoint = 'FULL';
+
+console.log({
+    name,hpPoint,isAlive
+});
+
+
+export {}

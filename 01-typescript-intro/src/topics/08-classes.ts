1756148a3d4eedf1f00d796d76f64e4262e1cf10
Christian <christiancatamuscay@r-fast.com>
2023-07-11 15:40:23 -0500

    Sección 3: Bases de TypeScript - Recomendado - 25. Priorizar composición sobre herencia

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -2,23 +2,39 @@ export class Person {
 
 
     constructor(
-        public name:string,
+        public firstName:string,
+        public lastName:string,
         private address:string = 'No Address'){
     }
 
 }
 
 
-export class Hero extends Person{
+// export class Hero extends Person{
 
+
+//     constructor( public alterEgo: string,
+//         public age:number,
+//         public realName:string
+//         ){
+//         super(realName,'New York');
+//     }
+
+// }
+
+
+export class Hero {
+    
     constructor( public alterEgo: string,
         public age:number,
-        public realName:string
+        public realName:string,
+        public person:Person
         ){
-        super(realName,'New York');
     }
 
 }
 
-const ironman = new Hero('IronMan',45,'Tony');
+const tony = new Person('Tony','Stark','New york');
+
+const ironman = new Hero('IronMan',45,'Tony',tony);
 console.log(ironman);

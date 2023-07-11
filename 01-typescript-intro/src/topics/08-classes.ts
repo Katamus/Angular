f64aa4dfb09396f749b4a3b15d72d250d47e5bc7
Christian <christiancatamuscay@r-fast.com>
2023-07-11 15:27:34 -0500

    Sección 3: Bases de TypeScript - Recomendado - 23. Constructor de una clase

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,11 +1,9 @@
 export class Person {
 
-    public name:string;
-    private address:string;
 
-    constructor(name:string,address:string){
-        this.name = name;
-        this.address = address;
+    constructor(
+        public name:string,
+        private address:string = 'No Address'){
     }
 
 }

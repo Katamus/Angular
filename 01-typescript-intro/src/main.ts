2e1201d180aac5b7f66da8c0dee3601981e950e5
Christian <christiancatamuscay@r-fast.com>
2023-07-11 16:05:33 -0500

    Sección 3: Bases de TypeScript - Recomendado - 27. Decoradores

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 // import './topics/01-basic-types';
 // import './topics/02-object-interfaces';
 //import './topics/07-import-export'
-import './topics/09-generics';
+import './topics/10-decorators';
 
 const app = document.querySelector<HTMLDivElement>("#app");
 

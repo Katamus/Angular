eacf0dffab22fb10ab6b1d56e72497e462e21f55
Christian <christiancatamuscay@r-fast.com>
2023-07-12 13:59:52 -0500

    Sección 5: Expandir Bases de Angular - 57. Pensemos en componentes pequeños

---
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-addcharacter',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+export class AddCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

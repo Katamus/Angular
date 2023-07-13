9dbfc3d2a6effda25ddb9fdf19ae761d25d4678e
Christian <christiancatamuscay@r-fast.com>
2023-07-13 15:27:02 -0500

    Sección 5: Expandir Bases de Angular - 68. Paquetes externos - UUID

--- 02-bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
-
+import { v4 as uuid } from "uuid";
 @Injectable({
     providedIn: 'root'
 })
@@ -8,23 +8,34 @@ export class DbzService {
 
     public characters: Character[] = [
         {
+            id: uuid(),
             name:'Krillin',
             power:1000
         },{
+            id: uuid(),
             name:'Goku',
             power:9500
         },{
+            id: uuid(),
             name:'Veggeta',
             power:7500
         }
     ];
 
     onNewCharacter(character:Character):void{
-        this.characters.push(character);
+        const newChracter : Character = {
+            ...character,
+            id:uuid()
+        }
+        this.characters.push(newChracter);
     }
 
-    onDeleteCharacter(index:number):void{
-        this.characters.splice(index,1);
+    // onDeleteCharacter(index:number):void{
+    //     this.characters.splice(index,1);
+    // }
+
+    deleteCharacterById(id:string){
+        this.characters = this.characters.filter(character => character.id !==id );
     }
 
 }

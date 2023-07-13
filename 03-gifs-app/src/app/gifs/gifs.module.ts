a1f0879b5a610937fe9889b147fc6b122c5b546b
Christian <christiancatamuscay@r-fast.com>
2023-07-13 17:28:22 -0500

    Sección 7: GifsApp - 84. Diseño de Gifs Components

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomepageComponent } from './page/homepage/homepage.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomepageComponent,
+    SearchBoxComponent
+  ],
   imports: [
     CommonModule
+  ],exports:[
+    HomepageComponent
   ]
 })
 export class GifsModule { }

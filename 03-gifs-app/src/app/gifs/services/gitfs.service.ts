50fda17445d51531508f5db572af10d7ca3957a7
Christian <christiancatamuscay@r-fast.com>
2023-07-15 15:50:55 -0500

    Sección 7: GifsApp - 90. Realizar una petición HTTP

--- 03-gifs-app/src/app/gifs/services/gitfs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gitfs.service.ts
@@ -1,18 +1,44 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+    constructor(private http:HttpClient){
+
+    }
+
+    private apikey:string = "IhY0KXdw7ZeHCvZIc6AmuJuKViHmYwbq";
+    private serviceUrl:string = "https://api.giphy.com/v1/gifs";
     private _tagHistory:string[] = [];
 
     get tagsHistory(){
         return [...this._tagHistory];
     }
 
-    searchTag(tag:string):void {
+    async searchTag(tag:string):Promise<void> {
         if(tag.length === 0) return;
 
         this.organizeHistory(tag);
+
+        /*
+        const resp = await fetch("https://api.giphy.com/v1/gifs/search?api_key=IhY0KXdw7ZeHCvZIc6AmuJuKViHmYwbq&q=valorant&limit=10");
+        const data = await resp.json();
+        console.log(data);
+       */ 
+      
+        const params = new HttpParams()
+            .set('api_key',this.apikey)
+            .set('limit',10)
+            .set('q',tag);
+        
+      this.http
+        .get(`${this.serviceUrl}/search`,{params})
+        .subscribe(resp=>{
+            console.log(resp);
+            
+        })
     }
 
     private organizeHistory (tag:string){

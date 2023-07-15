6cc2dafe98a332578156e82571565aaa943f1388
Christian <christiancatamuscay@r-fast.com>
2023-07-15 15:22:28 -0500

    Sección 7: GifsApp - 88. Validaciones al servicio

--- 03-gifs-app/src/app/gifs/services/gitfs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gitfs.service.ts
@@ -10,7 +10,18 @@ export class GifsService {
     }
 
     searchTag(tag:string):void {
+        if(tag.length === 0) return;
+
+        this.organizeHistory(tag);
+    }
+
+    private organizeHistory (tag:string){
+        tag = tag.toLowerCase();
+        if(this._tagHistory.includes(tag)){
+            this._tagHistory = this._tagHistory.filter((oldTag)=> oldTag != tag);
+        }
         this._tagHistory.unshift(tag);
+        this._tagHistory = this._tagHistory.splice(0,10);
     }
 
 }

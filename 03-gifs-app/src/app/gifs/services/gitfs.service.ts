35606feb922bb1b8b53ea78450c9c802e7498473
Christian <christiancatamuscay@r-fast.com>
2023-07-15 16:51:27 -0500

    Sección 7: GifsApp - 95. LocalStorage - Persistencia local y 96. Leer del LocalStorage

--- 03-gifs-app/src/app/gifs/services/gitfs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gitfs.service.ts
@@ -7,7 +7,7 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 export class GifsService {
 
     constructor(private http:HttpClient){
-
+        this.loadLocalStorage();
     }
 
     private apikey:string = "IhY0KXdw7ZeHCvZIc6AmuJuKViHmYwbq";
@@ -17,6 +17,10 @@ export class GifsService {
 
     get tagsHistory(){
         return [...this._tagHistory];
+    }
+
+    private saveLocalStorage():void{
+        localStorage.setItem('history', JSON.stringify(this._tagHistory));
     }
 
     searchTag(tag:string):void {
@@ -39,8 +43,6 @@ export class GifsService {
         .get<SearchResponse>(`${this.serviceUrl}/search`,{params})
         .subscribe(resp=>{
             this.gifList = resp.data;
-            console.log(this.gifList);
-            
         })
     }
 
@@ -51,6 +53,16 @@ export class GifsService {
         }
         this._tagHistory.unshift(tag);
         this._tagHistory = this._tagHistory.splice(0,10);
+        this.saveLocalStorage();
+    }
+
+    private loadLocalStorage():void{
+
+        if(!localStorage.getItem('history')) return;
+
+        this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+        this.searchTag(this._tagHistory[0]);
+        
     }
 
 }

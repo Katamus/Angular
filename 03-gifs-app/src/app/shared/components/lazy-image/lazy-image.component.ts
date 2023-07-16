856e0ea04934c63b851d65aec36901f8a503d625
Christian <christiancatamuscay@r-fast.com>
2023-07-16 07:05:32 -0500

    Sección 8: Image Loader - 104. Lazy Image - Parte 2

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrls: ['./lazy-image.component.css']
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+  
+  @Input()
+  public url!:string;
+
+  @Input()
+  public alt:string = '';
+
+  hasLoaded:boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('Method not implemented.');
+  }
+
+  onLoad(){
+    console.log("Cargo");
+    this.hasLoaded = true;
+  }
 
 }

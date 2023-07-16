48bddf6e9f93a8ee53a48b860974e04de1972c5d
Christian <christiancatamuscay@r-fast.com>
2023-07-16 09:49:47 -0500

    Sección 9: Country SPA - 115. LazyLoad - Carga perezosa

--- 04-countryApp/src/app/countries/countries-routing.module.ts
+++ 04-countryApp/src/app/countries/countries-routing.module.ts
@@ -15,7 +15,7 @@ const routes:Routes = [
         component:ByCountryPageComponent
     },
     {
-        path:"by-regionl",
+        path:"by-region",
         component:ByRegionPageComponent
     },
     {

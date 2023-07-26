3bc174a2bacf5701eeb2245486f9b60bfcb640f3
Christian <christiancatamuscay@r-fast.com>
2023-07-26 17:17:34 -0500

    Sección 16: Protección de Rutas - 222. Implementar la funcionalidad del Guard

--- 06-heroesApp/src/app/auth/guards/auth.guard.ts
+++ 06-heroesApp/src/app/auth/guards/auth.guard.ts
@@ -1,23 +1,32 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, RouterStateSnapshot, UrlSegment } from "@angular/router";
-import { Observable } from "rxjs";
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment } from "@angular/router";
+import { Observable, tap } from "rxjs";
+import { AuthService } from "../services/auth.service";
 
 @Injectable({providedIn:'root'})
 export class AuthGuard implements CanMatch, CanActivate {
 
-    constructor(){}
+    constructor(private authService:AuthService,private router:Router){}
+
+    private checkAuthStatus(): boolean | Observable<boolean>{
+       return this.authService.checkAuthentication()
+        .pipe(
+            tap((isAuthenticated)=>console.log(isAuthenticated)),
+            tap((isAuthenticated)=>{
+                if(!isAuthenticated){
+                    this.router.navigate(['./auth/login']);
+                }
+            }),
+        );
+    }
 
 
     canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
-        console.log("Can Match");
-        //throw new Error("Prueba de datos");
-        return true;
+        return this.checkAuthStatus();
     }
     
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
-        console.log("Can Activate");
-        //throw new Error("Prueba de datos");
-        return true;
+        return this.checkAuthStatus();
     }
 
 

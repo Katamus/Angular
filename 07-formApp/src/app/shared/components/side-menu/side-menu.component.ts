c2555eb50f04565b96e39a2da9a56e0228af720b
Christian <christiancatamuscay@r-fast.com>
2023-07-27 17:03:40 -0500

    Sección 17: Formularios Reactivos - 231. Barra de navegación

---
+++ 07-formApp/src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+
+interface MenuItem {
+  title:string;
+  route:string;
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: [
+  ]
+})
+export class SideMenuComponent implements OnInit {
+
+  public reactiveMenu: MenuItem[] = [
+    {title:'Basicos',route:'./reactive/basic'},
+    {title:'Dinámicos',route:'./reactive/dynamic'},
+    {title:'Switches',route:'./reactive/switches'},
+  ]
+
+  public authMenu: MenuItem[] = [
+    {title:'Registro',route:'./auth'},
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

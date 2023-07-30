389829d405c84828e4aff6433e4c3e4a377891d7
Christian <christiancatamuscay@r-fast.com>
2023-07-30 14:08:39 -0500

    Sección 18: Validaciones - 246. Solución de la tarea

--- 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -8,22 +8,43 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SwitchesPageComponent implements OnInit {
 
+  public person = {
+    gender: 'f',
+    wantNotificacions: false
+  };
+
   constructor(private fb:FormBuilder) { }
 
   public myForm: FormGroup = this.fb.group({
     gender: ['M', Validators.required],
     wantNotificacions:[true, Validators.required],
-    termsAndConditions: [false, Validators.requiredTrue]
+    termsAndConditions: [null, Validators.requiredTrue]
   })
 
   ngOnInit(): void {
+    this.myForm.reset({
+      ...this.person
+    })
   }
 
   onSave(){
     if(this.myForm.invalid){
-      this.myForm.markAllAsTouched;
+      this.myForm.markAllAsTouched();
       return;
     }
+
+
+    const {termsAndConditions, ...newPerson} = this.myForm.value;
+    this.person =  newPerson;
+
+    console.log(this.person);
+    console.log(this.myForm.value);
+  }
+
+  isValidField(field:string):boolean | null {
+    return this.myForm.controls[field].errors
+            && 
+            this.myForm.controls[field].touched;
   }
 
 }

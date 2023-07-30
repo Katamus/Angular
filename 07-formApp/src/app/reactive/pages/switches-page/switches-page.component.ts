5b3c3f7d732633705aa4cc65b2344c849fef16ad
Christian <christiancatamuscay@r-fast.com>
2023-07-30 11:54:58 -0500

    Sección 18: Validaciones - 245. Continuación de proyecto y tarea

--- 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotificacions:[true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  })
 
   ngOnInit(): void {
+  }
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched;
+      return;
+    }
   }
 
 }

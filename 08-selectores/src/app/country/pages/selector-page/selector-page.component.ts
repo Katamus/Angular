56ac3d20848788c71bea3a51b3f295ff504736e5
Christian <christiancatamuscay@r-fast.com>
2023-07-30 18:38:45 -0500

    Sección 19: Formularios Reactivos - Multiples selectores anidados - 264. Obtener países por region

--- 08-selectores/src/app/country/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/country/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -30,8 +31,11 @@ export class SelectorPageComponent implements OnInit {
 
   onRegionChanged():void {
     this.myForm.get('region')!.valueChanges
-    .subscribe(region=>{
-      console.log(region);
+    .pipe(
+      switchMap((region)=>this.countriesService.getCountryByRegion(region))
+    )
+    .subscribe(countries=>{
+      console.log(countries);
     })
   }
 
--- 08-selectores/src/app/country/services/countries.service.ts
+++ 08-selectores/src/app/country/services/countries.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Region, Country, SmallCountry } from '../interfaces/country.interface';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+  private baseUrl = 'https://restcountries.com/v3.1/';
+
+  constructor(private http:HttpClient) { }
 
   private _regions:Region[] = [
     Region.Africa,
@@ -23,6 +26,12 @@ export class CountriesService {
 
   getCountryByRegion(region:Region):Observable<SmallCountry[]>{
 
+    if(!region ) return of([]);
+
+    const url:string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+    return this.http.get<SmallCountry[]>(url).pipe(
+      tap(resp => console.log(resp))
+    );
   }
 
 

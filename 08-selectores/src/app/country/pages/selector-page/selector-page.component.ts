f4fb3b3e7d3bce2a419cdc4ff8545f7724457dfe
Christian <christiancatamuscay@r-fast.com>
2023-07-30 18:12:33 -0500

    Sección 19: Formularios Reactivos - Multiples selectores anidados - 262. Selector de regiones

--- 08-selectores/src/app/country/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-selector-page',
@@ -8,7 +10,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,
+    private countriesService:CountriesService
+    ) { }
 
   public myForm:FormGroup = this.fb.group({
     region: ['',Validators.required],
@@ -17,6 +21,10 @@ export class SelectorPageComponent implements OnInit {
   })
 
   ngOnInit(): void {
+  }
+
+  get regions():Region[] {
+    return this.countriesService.regions;
   }
 
 }
--- 08-selectores/src/app/country/services/countries.service.ts
+++ 08-selectores/src/app/country/services/countries.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,18 @@ import { Injectable } from '@angular/core';
 export class CountriesService {
 
   constructor() { }
+
+  private _regions:Region[] = [
+    Region.Africa,
+    Region.Americas,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania
+  ];
+
+  get regions():Region[] {
+    return [...this._regions]
+  }
+
+
 }

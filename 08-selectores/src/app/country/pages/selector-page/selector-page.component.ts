ae362a0a6bc1335aeb4d5edf8f0fb9c1adf42fb1
Christian <christiancatamuscay@r-fast.com>
2023-07-30 17:49:53 -0500

    Sección 19: Formularios Reactivos - Multiples selectores anidados - 260. Formulario reactivo - primer selector

--- 08-selectores/src/app/country/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+
+  public myForm:FormGroup = this.fb.group({
+    region: ['',Validators.required],
+    country: ['',Validators.required],
+    borders: ['',Validators.required],
+  })
 
   ngOnInit(): void {
   }

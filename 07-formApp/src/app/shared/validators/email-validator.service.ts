555a194a6befdcdac7a4545f1d1a11e2cbe6f007
Christian <christiancatamuscay@r-fast.com>
2023-07-30 15:32:09 -0500

    Sección 18: Validaciones - 252. Elaborar el validador de correo

--- 07-formApp/src/app/shared/validators/email-validator.service.ts
+++ 07-formApp/src/app/shared/validators/email-validator.service.ts
@@ -6,16 +6,33 @@ import { Observable, of,delay } from "rxjs";
 @Injectable({providedIn:"root"})
 export class EmailValidatorService implements AsyncValidator {
 
+    // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    //     const email = control.value;
+    //     return of({
+    //         emailTaken:true
+    //     }).pipe(
+    //         delay(2000)
+    //     )
+    // }
+
     validate(control: AbstractControl): Observable<ValidationErrors | null> {
         
         const email = control.value;
-        console.log({email});
-        
-        return of({
-            emailTaken:true
+        const httpCallObservable = new Observable<ValidationErrors|null>((subscriber)=>{
+            console.log({email});
+
+            if(email ==='criscahu@hotmail.com'){
+                subscriber.next({emailTaken:true});
+                subscriber.complete();
+                //return
+            }
+            subscriber.next(null);
+            subscriber.complete();
         }).pipe(
             delay(2000)
-        )
+        );
+        
+        return httpCallObservable;
 
     }
 

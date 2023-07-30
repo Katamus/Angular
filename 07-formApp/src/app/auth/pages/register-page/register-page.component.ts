d73ba83628dc756e10e14ec8c82e6c8a28932c9f
Christian <christiancatamuscay@r-fast.com>
2023-07-30 15:18:43 -0500

    Sección 18: Validaciones - 251. Validadores asíncronos

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorService } from 'src/app/shared/service/validators.service';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
@@ -12,7 +13,8 @@ export class RegisterPageComponent implements OnInit {
 
   constructor(
     private fb:FormBuilder,
-    private validatorService:ValidatorService
+    private validatorService:ValidatorService,
+    private emailValidator:EmailValidatorService
     ) { }
 
   ngOnInit(): void {
@@ -20,7 +22,8 @@ export class RegisterPageComponent implements OnInit {
 
   public myForm:FormGroup = this.fb.group({
     name: ['',[Validators.required,Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
-    email: ['',[Validators.required, Validators.pattern(this.validatorService.emailPattern) ]],
+    //email: ['',[Validators.required, Validators.pattern(this.validatorService.emailPattern) ],[ new EmailValidatorService()]],
+    email: ['',[Validators.required, Validators.pattern(this.validatorService.emailPattern) ],[ this.emailValidator]],
     username: ['',[Validators.required, this.validatorService.cantBeStrider]],
     password: ['',[Validators.required, Validators.minLength(6) ]],
     password2: ['',[Validators.required ]],

a7815e165761fc9bc4ae5096a7cfd32e3f616d5c
Christian <christiancatamuscay@r-fast.com>
2023-07-31 16:32:31 -0500

    Sección 20: LifeCycle Hooks - 273. Implementar todos los Lifecycle Hooks

--- 09-lifecycle/src/app/products/pages/product/product.component.ts
+++ 09-lifecycle/src/app/products/pages/product/product.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentChecked, AfterContentInit,AfterContentInit,AfterViewInit,AfterViewChecked,OnDestroy {
+
+  public isProductVisible = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+
+  ngOnChanges():void {
+    console.log("ngOnChanges");
+  }
+
+  ngDoCheck():void {
+    console.log("ngDoCheck");
+  }
+
+  ngAfterContentInit():void {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterViewInit():void {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked():void {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy():void {
+    console.log("ngOnDestroy");
+  }
+
+  ngAfterContentChecked():void {
+    console.log("ngAfterContentChecked");
   }
 
 }

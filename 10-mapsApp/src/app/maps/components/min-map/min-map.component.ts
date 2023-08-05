a4c9116767687716d7989b483b3a9af5e9831156
Christian <christiancatamuscay@r-fast.com>
2023-08-05 14:40:07 -0500

    Sección 21: Mapas en Angular - 298. Componente MiniMapa

--- 10-mapsApp/src/app/maps/components/min-map/min-map.component.ts
+++ 10-mapsApp/src/app/maps/components/min-map/min-map.component.ts
@@ -1,13 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {LngLat, Map, Marker} from 'mapbox-gl';
 
 @Component({
-  selector: 'app-min-map',
+  selector: 'maps-min-map',
   templateUrl: './min-map.component.html',
   styleUrls: ['./min-map.component.css']
 })
-export class MinMapComponent implements OnInit {
+export class MinMapComponent implements OnInit, AfterViewInit {
+
+  @Input() lngLat?: [number,number];
+
+  @ViewChild("map") divMap?:ElementRef;
+
+  public map?:Map;
 
   constructor() { }
+
+
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement) throw "Map Div not found";
+    if(!this.lngLat) throw "LngLat can`t be null"
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      interactive:false
+    });
+
+    this.createMarker(this.lngLat);
+  }
+
+  createMarker (lngLat:[number,number]) {
+    if(!this.map) return;
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const [lng,Lat] = lngLat;
+    this.addMarker(new LngLat(lng,Lat),color);
+  }
+
+  addMarker(lngLat:LngLat,color:string ){
+    if(!this.map) return;
+
+    const marker = new Marker({
+      color:color,
+      draggable:true
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+  }
 
   ngOnInit(): void {
   }

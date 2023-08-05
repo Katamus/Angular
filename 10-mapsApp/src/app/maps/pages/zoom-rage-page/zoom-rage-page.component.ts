1a1b9f9a168ac8d97eca61a12c9b5f57dd6bb490
Christian <christiancatamuscay@r-fast.com>
2023-08-05 11:23:07 -0500

    Sección 21: Mapas en Angular - 289. Controlar el nivel del Zoom

--- 10-mapsApp/src/app/maps/pages/zoom-rage-page/zoom-rage-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-rage-page/zoom-rage-page.component.ts
@@ -11,23 +11,53 @@ export class ZoomRagePageComponent implements AfterViewInit {
   
   @ViewChild('map') divMap?:ElementRef;
 
+  public zoom :number =10;
+  public map?:Map;
 
   constructor() { }
   ngAfterViewInit(): void {
 
     if(!this.divMap) throw 'El elmento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
       });
+
+      this.mapListeners();
   }
 
   ngOnInit(): void {
   }
 
+  mapListeners(){
 
+    if(!this.map) throw 'Mapa no inicializado';
 
+    this.map.on('zoom',(env)=> {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend',(env)=> {
+      if(this.map!.getZoom() < 18 ) return ;
+
+      this.map!.zoomTo(18);
+
+    });
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value:string){
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 }

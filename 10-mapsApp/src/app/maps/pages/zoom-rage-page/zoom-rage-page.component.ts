706798d453280796212ef6fdd006bfb87d9e8c7d
Christian <christiancatamuscay@r-fast.com>
2023-08-05 11:32:22 -0500

    Sección 21: Mapas en Angular - 290. Obtener las coordenadas centrales del mapa

--- 10-mapsApp/src/app/maps/pages/zoom-rage-page/zoom-rage-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-rage-page/zoom-rage-page.component.ts
@@ -1,20 +1,22 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import {Map} from 'mapbox-gl';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import {LngLat, Map} from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-rage-page',
   templateUrl: './zoom-rage-page.component.html',
   styleUrls: ['./zoom-rage-page.component.css']
 })
-export class ZoomRagePageComponent implements AfterViewInit {
+export class ZoomRagePageComponent implements AfterViewInit, OnDestroy {
 
   
   @ViewChild('map') divMap?:ElementRef;
 
   public zoom :number =10;
   public map?:Map;
+  public currentCenterLngLat: LngLat = new LngLat(-76.3017,3.5325);
 
   constructor() { }
+  
   ngAfterViewInit(): void {
 
     if(!this.divMap) throw 'El elmento HTML no fue encontrado';
@@ -22,11 +24,15 @@ export class ZoomRagePageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentCenterLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
       });
 
       this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   ngOnInit(): void {
@@ -42,10 +48,13 @@ export class ZoomRagePageComponent implements AfterViewInit {
 
     this.map.on('zoomend',(env)=> {
       if(this.map!.getZoom() < 18 ) return ;
-
       this.map!.zoomTo(18);
-
     });
+
+    this.map.on('move',()=>{
+      const {lng,lat} = this.currentCenterLngLat = this.map!.getCenter();
+
+    })
   }
 
   zoomIn(){

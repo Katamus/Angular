425d6b790cd414f1ac826e91e6f9d11e42a3a8c5
Christian <christiancatamuscay@r-fast.com>
2023-08-07 11:12:25 -0500

    Sección 23: Directivas personalizadas de Angular - 315. Cambiar el mensaje de la etiqueta

--- 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
+++ 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -9,9 +10,16 @@ export class CustomLabelDirective implements OnInit {
 
   private _color:string = 'red';
 
+  private _error?:ValidationErrors | null;
+
   @Input() set color(value:string){
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value:ValidationErrors | null | undefined){
+    this._error = value;
+    this.setErrorMessage();
   }
 
 
@@ -26,6 +34,20 @@ export class CustomLabelDirective implements OnInit {
   setStyle():void {
     if(!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage():void {
+    if(!this.htmlElement) return;
+    if(!this._error){
+      this.htmlElement.nativeElement.innerHTML = 'Hola mundo';
+      return;
+    } 
+    const errors = Object.keys(this._error);
+    console.log(errors);
+    if(errors.includes('required')){
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return
+    }
   }
 
 

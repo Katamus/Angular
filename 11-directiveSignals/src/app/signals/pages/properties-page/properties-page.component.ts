74f2aad51d0b8e077f743ce10929074d58e7d26e
Christian <christiancatamuscay@r-fast.com>
2023-08-07 16:29:13 -0500

    Sección 24: Signals en Angular - Angular 16+ - 329. Efectos con señales

--- 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,11 +1,22 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, computed, effect, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interfaces';
+import { count } from 'rxjs';
 
 @Component({
   templateUrl: './properties-page.component.html',
   styleUrls: ['./properties-page.component.css']
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy, OnInit {
+
+
+  ngOnInit(): void {
+    setInterval(()=>{
+      console.log("sss");
+      this.counter.update(counter => counter+1 );
+    },1000);
+  }
+
+  public counter = signal(10);
 
   public user = signal<User>({
     id: 2,
@@ -14,12 +25,23 @@ export class PropertiesPageComponent {
     last_name: "Weaver",
     avatar: "https://reqres.in/img/faces/2-image.jpg"
   });
-
+  
   public fullname = computed<string>(()=>{
     if(!this.user) return 'Usuario no encontrado';
-
     return `${this.user()?.first_name} ${this.user()?.last_name}`;
   });
+  
+  public userChangedEffect = effect(()=>{
+    console.log(`${this.user().first_name} - ${this.counter()}`);
+  });
+  
+  ngOnDestroy(): void {
+    //this.userChangedEffect.destroy();
+  }
+
+  increaseBy(value:number) {
+    this.counter.update(count =>count + 1);
+  }
 
   onFieldUpdated(field:keyof User,value:string){
     

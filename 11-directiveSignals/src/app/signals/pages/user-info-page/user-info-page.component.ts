3bc0ef2cf9090904bc4758f1f283c429ab94ab47
Christian <christiancatamuscay@r-fast.com>
2023-08-07 15:38:28 -0500

    Sección 24: Signals en Angular - Angular 16+ - 326. Manejo de errores y propiedades computadas

--- 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interfaces';
 
@@ -17,6 +17,12 @@ export class UserInfoPageComponent implements OnInit {
 
   public userWasFound = signal(true);
 
+  public fullname = computed<string>(()=>{
+    if(!this.currentUser) return 'Usuario no encontrado';
+
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
   ngOnInit(): void {
       this.loadUser(this.userId());
   }
@@ -29,8 +35,16 @@ export class UserInfoPageComponent implements OnInit {
     this.currentUser.set(undefined);
 
     this.userService.getUserbyId(id)
-      .subscribe(user=>{
-        this.currentUser.set(user);
+      .subscribe({
+        next:(user)=>{
+          this.currentUser.set(user);
+          this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        }
+
       })
 
   }

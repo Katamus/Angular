9a95a6fd35c1575aac5a8c0bb169d3c1ba877720
Christian <christiancatamuscay@r-fast.com>
2023-08-06 19:09:56 -0500

    Sección 22: Standalone Components - 303. Standalone - Como Página

---
+++ 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  //imports: [CommonModule],
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.css']
+})
+export class AlonePageComponent {
+
+  constructor() { }
+
+}

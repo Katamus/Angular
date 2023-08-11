e42b7a6d627d66f262210827752c63607373201e
Christian <christian.catamuscay.h@gmail.com>
2023-08-11 14:38:48 -0500

    Sección 25: Auth MEAN - Nest Backend - 342. Login - Verificar correo y contraseña

--- 12-nest-backeend/src/auth/auth.service.ts
+++ 12-nest-backeend/src/auth/auth.service.ts
@@ -1,10 +1,12 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { log } from 'console';
 
 @Injectable()
 export class AuthService {
@@ -41,6 +43,26 @@ export class AuthService {
       throw new BadRequestException(`${createUserDto.email} already exists`);
      }
      throw new InternalServerErrorException('Something terrible happen!!!');
+    }
+  }
+
+  async login(loginDto:LoginDto){
+
+    const {email,password} = loginDto;
+    const user = await this.userModel.findOne({email});
+    if(!user) {
+      throw new UnauthorizedException('Not valid credentials - email')
+    }
+
+    if(!bcryptjs.compareSync(password,user.password)){
+      throw new UnauthorizedException('Not valid credentials - password')
+    }
+
+    const {password:_, ...rest} = user.toJSON();
+
+    return {
+      user: rest,
+      token: 'ABC-123'
     }
   }
 

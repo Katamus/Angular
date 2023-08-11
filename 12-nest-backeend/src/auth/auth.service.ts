e9be1b9450981aec16bb91fb99236cfc7dc467ca
Christian <christian.catamuscay.h@gmail.com>
2023-08-11 11:01:40 -0500

    Sección 25: Auth MEAN - Nest Backend - 340. Crear un registro de base de datos

--- 12-nest-backeend/src/auth/auth.service.ts
+++ 12-nest-backeend/src/auth/auth.service.ts
@@ -1,12 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateUserDto) {
-    console.log(createAuthDto);
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name)
+    private userModel:Model<User>,
+  ){
+  }
+
+  create(createUserDto: CreateUserDto):Promise<User>{
+    try {
+      const newUser = new this.userModel(createUserDto);
+
+      // 1- Encriptar el password
+
+      // 2- Guardar el usuario
+
+      // 3- Generar el JWT
+
+      return newUser.save();
+    } catch (error) {
+      if(error.code === 1000){
+      throw new BadRequestException(`${createUserDto.email} already exists`);
+     }
+     throw new InternalServerErrorException('Something terrible happen!!!');
+    }
   }
 
   findAll() {

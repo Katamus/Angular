7825bf3ef5768321e581b1bd2cc6611e13a89319
Christian <christian.catamuscay.h@gmail.com>
2023-08-12 09:28:45 -0500

    Sección 26: AuthApp - Backend + Angular App - 359. Post - Login

--- 13-authApp/src/app/auth/services/auth-service.service.ts
+++ 13-authApp/src/app/auth/services/auth-service.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environments';
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
+export class AuthService {
 
   private readonly baseUrl: string = environment.baseUrl;
   private http = inject(HttpClient);
@@ -15,11 +15,23 @@ export class AuthServiceService {
   private _currentUser = signal<User|null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
-
+  public currentUser = computed(()=> this._currentUser());
+  public authStatus = computed(()=> this._authStatus());
 
   constructor() { }
 
   login(email:string, password:string):Observable<boolean>{
-    return of(true);
+    const url = `${this.baseUrl}/auth/login`;
+    const body = {email,password};
+
+    return this.http.post<LoginResponse>(url,body)
+      .pipe(
+        tap(({user,token})=>{
+          this._currentUser.set(user);
+          this._authStatus.set(AuthStatus.authenticated);
+          localStorage.setItem('token',token)
+        }),
+        map(()=>true)
+      );
   }
 }

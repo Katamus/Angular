11415c293e717ab0e4ace7e53f63d122d329bfc5
Christian <christian.catamuscay.h@gmail.com>
2023-08-13 07:51:44 -0500

    Sección 29: Bonus: Mapas - Marcadores y Direcciones con Mapbox - 386. Obtener la geolocalización del usuario

--- mapas-app/src/main.ts
+++ mapas-app/src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if(!navigator.geolocation){
+  alert('Navegador o soporta la Geolocation');
+  throw new Error('Navegador o soporta la Geolocation');
+}
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

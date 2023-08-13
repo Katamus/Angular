eb28090bc7dfed070685eca683ffa64e480fd9a1
Christian <christian.catamuscay.h@gmail.com>
2023-08-13 09:27:11 -0500

    Sección 29: Bonus: Mapas - Marcadores y Direcciones con Mapbox - 388. Mostrar un mapa de Mapbox

--- mapas-app/src/app/maps/components/map-view/map-view.component.ts
+++ mapas-app/src/app/maps/components/map-view/map-view.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
+import {Map} from 'mapbox-gl'
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent {
+export class MapViewComponent implements AfterViewInit {
+
+  @ViewChild('mapDiv')
+  mapDivElemt!: ElementRef;
 
   constructor( private placesService:PlacesService){
 
+  }
+  ngAfterViewInit(): void {
+    const map = new Map({
+      container: this.mapDivElemt.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.placesService.useLocation,
+      zoom: 14,
+      });
   }
 
 }
--- mapas-app/src/main.ts
+++ mapas-app/src/main.ts
@@ -2,11 +2,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import Mapbox from 'mapbox-gl';
+
+Mapbox.accessToken = 'pk.eyJ1IjoiY3IxNWNhaHUiLCJhIjoiY2xrcmx6bjQzMXI1bTNjbzU5eHRuNm1pMCJ9.A3LzUtPyJ46q3aHTD9fUXg';
+
 if(!navigator.geolocation){
   alert('Navegador o soporta la Geolocation');
   throw new Error('Navegador o soporta la Geolocation');
 }
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
